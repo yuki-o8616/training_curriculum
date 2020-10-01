4bad627097a34f23dec2ec4f711d5e1e9961c486
yuki-o8616 <0ku387431pf3g8d@ezweb.ne.kp>
2020-10-01 12:05:31 +0900

    issue1修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -43,7 +42,7 @@ class CalendarsController < ApplicationController
         wx = wx - 7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day,  :plans => today_plans, :wday => wdays[wx] }
+      days = { month: '(@todays_date + x)'.month, date: '(@todays_date+x)'.day,  plans:  'today_plans', wday:  'wdays[wx]' }
       @week_days.push(days)
     end
 
